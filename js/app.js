b5805797e00c4772d79da5fe9a8b146ff0e33ffd
Sahil Jain <sahiljainpkl199724@gmail.com>
2018-02-08 00:54:29 +0530

    reset done timer left

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
 var arr =  ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb','fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb']
 var previous;
 var previouscard;
@@ -8,14 +5,14 @@ var current;
 var currentcard;
 var count=0;
 var moves=0;
-var stars=document.getElementsByClassName('stars');
-console.log(stars);
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+var star1=1;
+var star2=1;
+var over=0;
+var stars=document.getElementsByClassName('fa-star');
+var cards=document.getElementsByClassName('card');
+var reset=document.getElementsByClassName('restart');
+reset[0].addEventListener("click",newgame);
+newgame();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -32,37 +29,26 @@ function shuffle(array) {
     return array;
 }
 
-var newarr=shuffle(arr);
-console.log(newarr);
-var cards=document.getElementsByClassName('card');
-//console.log(cards[0].childNodes[1]);
-for(var i =0;i<cards.length;i++){
-    var e=document.createElement('i');
-    e.classList.add('fa');
-    e.classList.add(newarr[i]);
-    cards[i].replaceChild(e,cards[i].childNodes[1]);
-    cards[i].classList.remove('open');
-    cards[i].classList.remove('show');
-    cards[i].classList.remove('match');
-    cards[i].addEventListener('click',opencard);
-}
-//console.log(cards[0].childNodes[1]);
 function opencard(){
-    //console.log('adad');
-
-  if(count==1){
-    moves=moves+1;
-    document.getElementsByClassName('moves')[0].innerHTML=moves;
-    for(var i=0;i<cards.length;i++){
-    cards[i].removeEventListener("click",opencard);  
-    }
-    this.style.transition="all 1s";
+    if(count==1){
+        moves=moves+1;
+        if (moves > 15 && star1 == 1){
+            stars[0].parentNode.removeChild(stars[0]);
+            star1=0;
+        }
+        if (moves >25 && star2 ==1){
+            stars[1].parentNode.removeChild(stars[1]);
+            star2=0;
+        }
+        document.getElementsByClassName('moves')[0].innerHTML=moves;
+        for(var i=0;i<cards.length;i++){
+            cards[i].removeEventListener("click",opencard);  
+        }
+        this.style.transition="all 1s";
         this.classList.add("open");
         this.classList.add("show");
         current=this.childNodes[1].className;
         currentcard=this;
-        //console.log(currentcard);
-        //console.log(current);
         count=0;
         if (current == previous){
             previouscard.classList.add("match");
@@ -76,53 +62,86 @@ function opencard(){
              }
         }
         else{
-          setTimeout(function() {
-            previouscard.style.background="#FF2D00";
-            currentcard.style.background="#FF2D00";
             setTimeout(function() {
-              previouscard.classList.remove("open");
-              previouscard.classList.remove("show");
-              previouscard.style.background=null;
-              currentcard.classList.remove("open");
-              currentcard.classList.remove("show");
-              currentcard.style.background=null;
-              previouscard=null;
-              previous=null;
-              currentcard=null;
-              current=null;
-              for(var i=0;i<cards.length;i++){
-                  cards[i].addEventListener("click",opencard);  
-              }
-             }, 1000);
-          }, 1000);
+                previouscard.style.background="#FF2D00";
+                currentcard.style.background="#FF2D00";
+                    setTimeout(function() {
+                        previouscard.classList.remove("open");
+                        previouscard.classList.remove("show");
+                        previouscard.style.background=null;
+                        currentcard.classList.remove("open");
+                        currentcard.classList.remove("show");
+                        currentcard.style.background=null;
+                        previouscard=null;
+                        previous=null;
+                        currentcard=null;
+                        current=null;
+                        for(var i=0;i<cards.length;i++){
+                            cards[i].addEventListener("click",opencard);  
+                        }
+                    }, 500);
+            }, 500);
         }
-    console.log(moves);
-    return; 
-  }
-  
-  if(count==0){
-    moves=moves+1;
-    document.getElementsByClassName('moves')[0].innerHTML=moves;
-    this.style.transition="all 1s";
+        console.log(moves);
+        for(var i =0;i<cards.length;i++){
+            if(cards[i].classList.contains('match'))
+                over=0;
+            else{
+                over=1;
+                break;
+            }
+        }
+        if(over==0){
+            console.log('done');
+        }
+        return; 
+    }
+    if(count==0){
+        moves=moves+1;
+        if (moves > 16 && star1 == 1){
+            stars[0].parentNode.removeChild(stars[0]);
+            star1=0;
+        }
+        if (moves >25 && star2 ==1 ){
+            star2=0;
+            stars[1].parentNode.removeChild(stars[1]);
+        }
+        document.getElementsByClassName('moves')[0].innerHTML=moves;
+        this.style.transition="all 1s";
         this.classList.add("open");
         this.classList.add("show");
+        this.removeEventListener("click",opencard);
         previous=this.childNodes[1].className;
         previouscard=this;
-        //console.log(previouscard);
-        //console.log(previous);
         count=count+1;
-        //console.log(count);
-  }    
-  
+    }      
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
+function newgame(){
+    var newarr=shuffle(arr);
+    for(var i =0;i<cards.length;i++){
+        var e=document.createElement('i');
+        e.classList.add('fa');
+        e.classList.add(newarr[i]);
+        cards[i].replaceChild(e,cards[i].childNodes[1]);
+        cards[i].addEventListener('click',opencard);
+        cards[i].classList.remove("show");
+        cards[i].classList.remove("open");
+        cards[i].classList.remove(",match");
+    }
+    document.getElementsByClassName('moves')[0].innerHTML=0;
+    var ee=document.createElement('i');
+    ee.classList.add('fa');
+    ee.classList.add('fa-star');
+    var list1=document.createElement('li');
+    list1.appendChild(ee);
+    var list2=list1.cloneNode(true);
+    var list3=list1.cloneNode(true);
+    var stars=document.getElementsByClassName('stars');
+    while (stars[0].hasChildNodes()) {
+        stars[0].removeChild(stars[0].lastChild);
+    }
+    stars[0].appendChild(list1);
+    stars[0].appendChild(list2);
+    stars[0].appendChild(list3);
+}
